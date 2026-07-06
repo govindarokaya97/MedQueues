a77b8b88dd752d175636ecd9c796ff76a1499f40
govindarokaya97-commits <govindarokaya97@gmail.com>
2026-07-06 07:07:12 +0545

    fixing dashboard due to templates error

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def dashboard_view(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'dashboard/dashboard.html')
