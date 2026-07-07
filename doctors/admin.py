d23a2840ba623fb630504a3eb31a0ece6147930f
govindarokaya97-commits <govindarokaya97@gmail.com>
2026-07-07 13:57:09 +0545

    doctor and department model + Admin

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Doctor, Department
 
 # Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'department', 'specialization', 'experience', 'available')
+    list_filter = ('department', 'available')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'specialization')
