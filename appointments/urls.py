4da54423ab3d77ca6b8323d064d41fa9da5ebc49
govindarokaya97-commits <govindarokaya97@gmail.com>
2026-07-08 16:50:09 +0545

    Appointment CRUD

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.appointment_list, name="appointments_list"),
     path("add/", views.appointment_create, name="appointments_add"),
+    path("<int:id>/", views.appointment_detail, name="appointments_detail"),
+    path("<int:id>/edit/", views.appointment_update, name="appointments_edit"),
+    path("<int:id>/delete/", views.appointment_delete, name="appointments_delete"),
+
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 from .models import Appointment
 from .forms import AppointmentForm
 from django.contrib import messages
@@ -12,10 +13,31 @@ def appointment_list(request):
         "doctor__user",
     )
 
+    search = request.GET.get("search")
+    status = request.GET.get("status")
+    date = request.GET.get("date")
+
+    if search:
+        appointments = appointments.filter(
+            Q(patient__first_name__icontans=search) |
+            Q(patient__last_name__icontans=search) |
+            Q(doctor__user__first_name__icontans=search) |
+            Q(doctor__user__last_name__icontans=search)    
+        )
+    if status:
+        appointments = appointments.filter(status=status)
+    
+    if date:
+        appointments = appointments.filter(appointment_date=date)
+        
+
     return render(
         request, 
         "appointments/appointments_list.html",
-        {"appointments":appointments}
+        {
+            "appointments":appointments,
+            "status_choices": Appointment.STATUS_CHOICES
+        }
     )
 
 def appointment_create(request):
@@ -36,3 +58,44 @@ def appointment_create(request):
         "appointments/appointments_form.html",
         {"form":form}
     )
+
+def appointment_detail(request, id):
+    appointment = get_object_or_404(Appointment, id=id)
+
+    return render(request,"appointments/appointments_detail.html",{
+        "appointment":appointment
+    })
+
+
+def appointment_update(request, id):
+    appointment = get_object_or_404(Appointment, id=id)
+
+    if request.method == "POST":
+        form = AppointmentForm(request.POST, instance=appointment)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Edited Booked Appointment")
+            return redirect("appointments_list")
+    
+    else:
+        form = AppointmentForm(instance=appointment)
+    
+    return render(
+        request, 
+        "appointments/appointments_form.html",
+        {"form":form}
+    )
+
+
+def appointment_delete(request, id):
+    appointment = get_object_or_404(Appointment, id=id)
+
+    if request.method == "POST":
+        appointment.delete()
+        messages.success(request, "Removed Booked Appointment")
+        return redirect("appointments_list")
+    
+    return render(request,"appointments/appointments_confirm_delete.html",{
+        "appointment":appointment
+    })
